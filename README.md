baf2f43dbd95c86851ab2f39e46d227610a95f3f
Georgia A <and.georgia.m@gmail.com>
2014-05-27 20:57:04 -0700

    Update ruby-version to 2-p353 Update README make it less hardcoded

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # PivotalTracker to Github Issues migration scripts
 
-To succesful migrate tickets from PivotalTracker to Github do:
+Rewrote the original import_issues.rb into import_octokit.rb to use github's official ruby gem. The original script wasn't working for me.
+
+To succesfully migrate tickets from PivotalTracker to Github do:
 
 1. Clone this repo
-2. Run in console: `gem install httparty`
+2. Install the gems octokit and highline
 3. Import all user stories from PivotalTracker to csv file
-4. Name it `issues.csv`
-5. If you need to clear all existing repo labels, run: `ruby delete_labels.rb`, but check `your_github_username/your_repository_name` block inside the file first
-6. For importing tickets from csv to repo, run: `ruby import_issues.rb issues.csv` and follow prompts
+4. For importing tickets from csv to repo, run: `ruby import_octokit.rb your-issues.csv` and follow prompts
 
-Thanks for making these scripts to @robotarmy and his gist: https://gist.github.com/2257596
+I haven't looked at the delete_labels script yet, it could probably use some work.
+
+Original thanks for making these scripts to @robotarmy and his gist: https://gist.github.com/2257596
 
 Done!
--- import_octokit.rb
+++ import_octokit.rb
@@ -1,26 +1,53 @@
 #encoding:UTF-8
 #!/usr/bin/env ruby
 require 'OctoKit'
-require 'yaml'
 require 'CSV'
+require 'highline/import'
 
-LOGIN = YAML.load_file('login.yaml')
+# requires fiddling with the labels later, not as neat with those
+# my csv didn't have any of the comments, so didn't have to handle those?
+
+def get_input(prompt="Enter >",show = true)
+	ask(prompt) {|q| q.echo = show}
+end
+
+issues_csv = ARGV.shift or raise "Enter Filepath to CSV as ARG1"
+
+user = get_input('Enter Username >')
+password = get_input('Enter Password >', '*')
 
 client = Octokit::Client.new \
-	:login => LOGIN['USERNAME'],
-	:password => LOGIN['PASSWORD']
+	:login => user,
+	:password => password
 
 user = client.user
 user.login
 
-repo = 'antislice/HelloRubyTuesdays'
+repo = get_input('Enter repo (owner/repo_name) >')
+
+Issue = Struct.new(:title, :body, :labels, :comment)
+issues = Array.new
 
 CSV.foreach 'hrt_issues.csv', headers: true do |row|
 	labels = row['Labels'].split(',')
 	labels << row['Story Type']
-	client.create_issue(repo, row['Story'], row['Description'], {:labels => labels.join(',')})
+	comment = row['Comment'].nil? ? nil : row['Comment']
+	issues << Issue.new(row['Story'], row['Description'], labels, comment)
 end
 
+unique_labels = issues.map{ |i| i.labels.each{|l| l.trim} }.uniq
+puts unique_labels.to_s
+unique_labels.each do |l|
+    color = ''
+    3.times { color << "%02x" % rand(255) }
+    puts l.to_s
+    puts color.to_s
+	#client.add_label(repo, l, color)
+end
 
-#issue = client.create_issue("antislice/HelloRubyTuesdays", 'test API issue', 'test issue body', {:labels => "bug"})
-#puts issue.title
+issues.each do |issue|
+	puts "creating issue '#{issue.title}'"
+	client.create_issue(repo, issue.title, issue.body, {:labels => issue.labels})
+end
+
+# TODO add comments
